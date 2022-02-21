43daf408f9c3923019ca8b94e92b58551a587304
Patricia Eliseu <patyeliseu4@gmail.com>
2022-02-21 14:37:38 -0300

    atualizar e nono requisito

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let corPalette = document.getElementsByClassName('color');
 
 let penColour = "black";
 
-function setPenColout(pen) {
+function setPenColour(pen) {
     penColour = pen;
+}
+
+
+let selectColor = document.getElementsByClassName('color').addEventListerner('select', nomeDaFuncao);
+
+function nomeDaFuncao() {
+
 }
