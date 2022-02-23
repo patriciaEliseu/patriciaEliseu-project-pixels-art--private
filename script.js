058734831bf2fafd207fcfcaf869e37b6fd3e7bf
Patricia Eliseu <patyeliseu4@gmail.com>
2022-02-23 11:29:15 -0300

    adicionei oitavo requisito

--- script.js
+++ script.js
@@ -22,20 +22,32 @@ let pixelsColor = document.getElementsByClassName('pixel')
 for (let index = 0; index < pixelsColor.length; index += 1) {
     /*  console.log(pixelsColor[index]); */
     pixelsColor[index].addEventListener('pixel', function(event) {
+        console.log(event.target);
         let selected = document.getElementsByClassName('selected')[0];
-        event.target.style.backgroundColor = selectedcolor;
+        event.target.style.backgroundColor = selected;
+        color;
     })
 }
 
-
-
-/* console.log(colours); */
-/* let button = document.getElementById('clear-board'); */
-/* console.log(button); */
-/* utton.addEventListener('click', function(event) {
-    event
-}) */
-
-/* console.log('teste'); */
-/* let quadro = document.getElemensByClassName('linha');
-console.log(quadro); */
+/* let carregarJanela=window.getComputedStyle(selected, null); */
+function mudarPintarCor() {
+    let paintPixel = document.getElementsByClassName('pixel')
+    for (let index = 0; index < paintPixel.length; index += 1) {
+        /*  console.log(paintPixel[index]); */
+        paintPixel[index].addEventListener('click', function(event) {
+            let selected = document.querySelector('.selected');
+            /* console.log(selected.style.backgroundColor); */
+            let cor = getComputedStyle(selected).backgroundColor;
+            console.log(cor);
+            event.target.style.backgroundColor = cor;
+        })
+    }
+}
+mudarPintarCor();
+/* let pintarPixel = document.getElementsByClassName('pixel') */
+/* console.log(pintarPixel); */
+/* function pintarPixels(pintarPixel) { */
+/* let pintarPixel = document.getElementsByClassName('pixel') */
+/* pintarPixels.style.backgroundColor = ('black');
+    console.log(pintarPixels);
+} */
